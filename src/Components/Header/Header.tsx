bba821f384cc36b15974ca155462e86a5ceacbe3
gabrielCos <chris-19121@hotmail.com>
2025-01-20 13:29:08 -0300

    Inserção de logo e animação de rotação

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,15 @@ import styles from "./Header.module.css";
 const Header: React.FC = () => {
     return (
         <div className={styles.header}>
-
+            <div className={styles.headerElements}>
+                <div className={styles.logo}>
+                    <div className={`${styles.circle} ${styles.green}`}></div>
+                    <div className={styles.logoElements}>
+                        <div className={`${styles.circle} ${styles.blue}`}></div>
+                        <div className={`${styles.circle} ${styles.orange}`}></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

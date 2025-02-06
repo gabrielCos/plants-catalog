411b37240f86d7034db37b52bddb490883959a70
gabrielCos <chris-19121@hotmail.com>
2025-02-06 19:01:37 -0300

    Ajustes na parte específica da planta

--- src/Components/PlantSpecific/PlantSpecific.tsx
+++ src/Components/PlantSpecific/PlantSpecific.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import styles from "./PlantSpecific.module.css";
 
+import plantSpecific from "../../Static/Images/plantSpecific.png"
+
 import { ArrowLeftOutlined } from '@ant-design/icons';
 
 const PlantSpecific: React.FC = () => {
@@ -15,6 +17,21 @@ const PlantSpecific: React.FC = () => {
                 <div className={styles.columnOne}>
                     <p className={styles.plantName}>Chambá (Anador, Trevo-Cumaru)</p>
                     <p className={styles.scientificName}>Justicia pectoralis</p>
+                    <img src={plantSpecific} />
+                    <p className={styles.indicationsLabel}>Indicação</p>
+                    <div className={styles.indicationsContainer}>
+                        <p className={styles.indications}>Crises de asma</p>
+                        <p className={styles.indications}>Tosse</p>
+                        <p className={styles.indications}>Bronquite</p>
+                        <p className={styles.indications}>Chiado no peito</p>
+                    </div>
+                </div>
+                <div className={styles.columnTwo}>
+                    <div className={styles.prepareContainer}>
+                        <div className={styles.instructions}>
+                            <p className={styles.instructionsLabel}>Modo de preparo</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

54f5cba78b8156712ebd442a5ea918ff8f892ca9
gabrielCos <chris-19121@hotmail.com>
2025-02-07 15:52:38 -0300

    Finalização da página de especificidades

--- src/Components/PlantSpecific/PlantSpecific.tsx
+++ src/Components/PlantSpecific/PlantSpecific.tsx
@@ -30,6 +30,26 @@ const PlantSpecific: React.FC = () => {
                     <div className={styles.prepareContainer}>
                         <div className={styles.instructions}>
                             <p className={styles.instructionsLabel}>Modo de preparo</p>
+                            <p className={styles.instructionsTitle}>Chá de Chambá</p>
+                            <p className={styles.ingredientsLable}>Ingredientes:</p>
+                            <ul className={styles.ingredients}>
+                                <li>1 copo de água fervida ou filtrada</li>
+                                <li>1 xíxara e meia de folhas de chambá (bem lavadas e cortadas em pedaços)</li>
+                            </ul>
+                            <p className={styles.ingredientsLable}>Passos:</p>
+                            <ol className={styles.ingredients}>
+                                <li>Cozinhe as folhas até que exalem um aroma parecido com o do cumaru.</li>
+                                <li>Em seguida, coe as folhas e reserve o chá em um recipiente.</li>
+                            </ol>
+                            <p className={styles.ingredientsLable}>Recomendação de Uso:</p>
+                            <p className={styles.use}>Consumir o chá 2x ao dia e manter o tratamento durante 1 semana</p>
+                        </div>
+                    </div>
+                    <div className={styles.prepareContainer}>
+                        <div className={styles.instructions}>
+                            <p className={styles.contraindicationLabel}>Contra indicações</p>
+                            <p className={styles.contraindications}>Crianças menores de 6 anos, Gestantes, Lactantes.</p>
+                            <p className={styles.contraindications}>Não deve ser usada conjuntamente com anticoagulantes ou em pacientes com transtornos circulatórios</p>
                         </div>
                     </div>
                 </div>
